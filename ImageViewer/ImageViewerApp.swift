33a9d111dbf3c69fbf01171f213ab6d4e61542a4
dorian <daoyuan0626@gmail.com>
2025-11-25 23:01:22 +0800

    Initial Commit

---
+++ ImageViewer/ImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerApp.swift
+//  ImageViewer
+//
+//  Created by Dorian Wang on 2025/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
